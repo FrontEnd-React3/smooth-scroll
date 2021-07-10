da86df067b8c3ca58aa1478886f46ce72badcca9
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2021-07-10 13:52:46 +0200

    e

---
+++ src/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { links } from './data'
+const Navbar = () => {
+  const handleClick = (e) => {
+    e.preventDefault()
+    const target = e.target.getAttribute('href')
+    const location = document.querySelector(target).offsetTop
+
+    window.scrollTo({
+      left: 0,
+      top: location - 64,
+    })
+  }
+  return (
+    <nav className='navbar sticky'>
+      <div className='nav-center'>
+          {links.map((link) => {
+            return (
+              <a href={link.url} key={link.id} onClick={handleClick}>
+                {link.text}
+              </a>
+            )
+          })}
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
